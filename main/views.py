fd8e409c7d6b1cc2031f8849ab477bab6b84cb28
wqazx8517@gmail.com <102226122+nyeon0000@users.noreply.github.com>
2022-05-03 12:45:13 +0900

    ny.html

--- main/views.py
+++ main/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 
 
 # mainpage view 함수
-
+def showmain(request):
+    return render(request, 'main/mainpage.html')
 
 # firstpage view 함수
-
+def showfirst(request):
+    return render(request, 'main/firstpage.html')
 
 # secondpage view 함수
+def showsecond(request):
+    return render(request,'main/secondpage.html')
+
 
