389f374e4a4e72b6adb9a6670ddc6d23182295cd
wqazx8517@gmail.com <102226122+nyeon0000@users.noreply.github.com>
2022-05-17 23:33:51 +0900

    5session

---
+++ main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+
+app_name= "main"
+urlpatterns=[
+    path('', main, name="showmain"),
+    path('showfirst/',showfirst, name="showfirst"),
+    path('showsecond/',showsecond, name="showsecond"),
+    path('posts/', posts, name="posts"),
+    path('<str:id>', detail, name="detail"),
+    path('new/',new, name="new"),
+    path('create/',create,name="create"),
+    path('posts/', posts, name="posts"),
+    path('edit/<str:id>',edit, name="edit"),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>',delete, name="delete"),
+
+]
--- main/views.py
+++ main/views.py
@@ -3,9 +3,13 @@ from .models import Post
 from django.utils import timezone
 
 # mainpage view 함수
-def showmain(request):
+def main(request):
     posts=Post.objects.all()
     return render(request, 'main/mainpage.html',{'posts':posts})
+
+def posts(request):
+    posts=Post.objects.all()
+    return render(request, 'main/posts.html',{'posts':posts})
 
 # firstpage view 함수
 def showfirst(request):
@@ -28,6 +32,24 @@ def create(request):
     new_post.writer=request.POST['writer']
     new_post.pub_date=timezone.now()
     new_post.body=request.POST['body']
+    new_post.image=request.FILES.get('image')
     new_post.save()
-    return redirect('detail',new_post.id)
-    
+    return redirect('main:detail',new_post.id)
+
+def edit(request, id):
+    edit_post=Post.objects.get(id=id)
+    return render(request, 'main/edit.html',{'post': edit_post})
+
+def update(request, id):
+    update_post=Post.objects.get(id=id)
+    update_post.title = request.POST['title']
+    update_post.writer = request.POST['writer']
+    update_post.pub_date = timezone.now()
+    update_post.body = request.POST['body']
+    update_post.save()
+    return redirect('main:detail', update_post.id)
+
+def delete(request,id):
+    delete_post=Post.objects.get(id=id)
+    delete_post.delete()
+    return redirect('main:posts')
