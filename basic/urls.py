de19b236a032dd7f067aa458bc557ddc39174798
wqazx8517@gmail.com <102226122+nyeon0000@users.noreply.github.com>
2022-05-24 22:35:40 +0900

    session6 asi

--- basic/urls.py
+++ basic/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #mainpage URL 연결하기 with 별명사용
     path('', include('main.urls')),
+    path('accounts/',include('allauth.urls')),
+    path('users/',include('users.urls')),
+    
  ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -29,12 +29,12 @@ def new(request):
 def create(request):
     new_post=Post()
     new_post.title=request.POST['title']
-    new_post.writer=request.POST['writer']
+    new_post.writer=request.user
     new_post.pub_date=timezone.now()
     new_post.body=request.POST['body']
     new_post.image=request.FILES.get('image')
     new_post.save()
-    return redirect('main:detail',new_post.id)
+    return redirect('main:detail', new_post.id)
 
 def edit(request, id):
     edit_post=Post.objects.get(id=id)
@@ -43,7 +43,7 @@ def edit(request, id):
 def update(request, id):
     update_post=Post.objects.get(id=id)
     update_post.title = request.POST['title']
-    update_post.writer = request.POST['writer']
+    update_post.writer = request.user
     update_post.pub_date = timezone.now()
     update_post.body = request.POST['body']
     update_post.save()
