c8e41b5022e5f6ca87a9e003a7c3b5da5faa1afd
wqazx8517@gmail.com <102226122+nyeon0000@users.noreply.github.com>
2022-06-21 19:13:53 +0900

    session 7 asi

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
 
 # mainpage view 함수
@@ -21,7 +21,8 @@ def showsecond(request):
 
 def detail(request,id):
     post=get_object_or_404(Post,pk=id)
-    return render(request,'main/detail.html',{'post':post})
+    all_comments=post.comments.all().order_by('-created_at')
+    return render(request,'main/detail.html',{'post':post, 'comments':all_comments})
 
 def new(request):
     return render(request,'main/new.html')
@@ -53,3 +54,38 @@ def delete(request,id):
     delete_post=Post.objects.get(id=id)
     delete_post.delete()
     return redirect('main:posts')
+
+def create_comment(request, blog_id):
+    new_comment = Comment()
+    new_comment.writer = request.user
+    new_comment.content = request.POST['content']
+    new_comment.blog = get_object_or_404(Post, pk = blog_id)
+    new_comment.save() 
+    return redirect('main:detail', blog_id)
+
+def create_comment(request, id):
+    if request.method == "POST":
+        post = get_object_or_404(Post, pk=id)
+        current_user = request.user
+        comment_content = request.POST.get("content")
+        Comment.objects.create(content=comment_content, writer=current_user, post=post)
+    return redirect("main:detail", id)
+
+def edit_comment(request, id):
+    comment = Comment.objects.get(id=id)
+    if request.user == comment.writer:
+        return render(request, "main/edit_comment.html", {"comment": comment})
+    else:
+        return redirect("main:detail", comment.post.id)
+
+def update_comment(request, id):
+    comment = Comment.objects.get(id=id)
+    comment.content = request.POST.get("content")
+    comment.save()
+    return redirect("main:detail", comment.post.id)
+
+def delete_comment(request, id):
+    comment = Comment.objects.get(id=id)
+    if request.user == comment.writer:
+        comment.delete()
+    return redirect("main:detail", comment.post.id)
